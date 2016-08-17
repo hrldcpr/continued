3f37c433b64e824fd63b96c8cab52f8dfff4999d
Harold Cooper <hrldcpr@gmail.com>
2016-08-17 15:53:30 -0400

    friendly instructions for terminal users

--- as_digits.py
+++ as_digits.py
@@ -7,5 +7,8 @@ def coefficients():
     for line in sys.stdin:
         yield int(line)
 
+if sys.stdin.isatty():
+    sys.stderr.write('Enter one coefficient per line.\n'
+                     'Finish with Control-D:\n')
 for d in continued.as_digits(coefficients()):
     print(d, end='', flush=True)
--- continued_digits.py
+++ continued_digits.py
@@ -10,5 +10,9 @@ def digits():
         try: yield int(c)
         except: yield c
 
+if sys.stdin.isatty():
+    sys.stderr.write('Enter digits followed by a decimal point followed by digits.\n'
+                     'Use Control-D any time to flush the terminal input,\n'
+                     'and Control-D Control-D to finish:\n')
 for a in continued.continued_digits(digits()):
     print(a)
