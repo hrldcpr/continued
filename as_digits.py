ea2b0e0387ebe92bade876103709fc725ec337f6
Harold Cooper <hrldcpr@gmail.com>
2016-09-01 16:59:19 -0400

    separate input from output when converting coefficients to digits in terminal

--- as_digits.py
+++ as_digits.py
@@ -7,8 +7,13 @@ def coefficients():
     for line in sys.stdin:
         yield int(line)
 
+
 if sys.stdin.isatty():
+    pretty = sys.stdout.isatty()
     sys.stderr.write('Enter one coefficient per line.\n'
                      'Finish with Control-D:\n')
+else: pretty = False
+
 for d in continued.as_digits(coefficients()):
-    print(d, end='', flush=True)
+    if pretty: print('\t', d)
+    else: print(d, end='', flush=True)
